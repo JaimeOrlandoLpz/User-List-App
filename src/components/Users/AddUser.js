12ce4c2cbdaf41c6977e81c107275b8b70f69285
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-11-29 12:27:57 -0600

    Adding States to Age and Username

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import styles from './AddUser.module.css'
 
 const AddUser = (props) => {
+    const [enteredUserName, setEnteredUserName] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
+
     const addUserEventHandler = (event) =>{
         event.preventDefault(); 
+        console.log(enteredAge, enteredUserName);
+
+    }
+
+    const usernameChangeHandler = (event) => {
+        setEnteredUserName(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
     }
 
     return(
     <Card className = {styles.input}>
         <form onSubmit={addUserEventHandler}>
             <label htmlFor="username">Username</label>
-            <input id = "username" type="text"/>
+            <input id = "username" type="text" onChange = {usernameChangeHandler}/>
             <label htmlFor="age">Age in years</label>
-            <input id = "age" type="number"/>
+            <input id = "age" type="number" onChange = {ageChangeHandler}/>
             <Button type = "submit">
                 Add User
             </Button>
