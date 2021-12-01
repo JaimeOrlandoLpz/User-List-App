393ded856428f6f3dd84bef05e11b43ff90418e1
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-12-01 12:39:29 -0600

    Changing age and username from state approach to ref approach

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,32 +1,47 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import ErrorModal from '../UI/ErrorModal';
 import styles from './AddUser.module.css'
 
 const AddUser = (props) => {
-    const [enteredUserName, setEnteredUserName] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
+    // const [enteredUserName, setEnteredUserName] = useState('');
+    // const [enteredAge, setEnteredAge] = useState('');
+
     const [error, setError] = useState();
 
     const addUserEventHandler = (event) =>{
         event.preventDefault(); 
-        if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
+        const enteredName = nameInputRef.current.value; // Access directly tbe value of the input DOM element
+        const enteredUserAge = ageInputRef.current.value;
+
+        if(enteredName.trim().length === 0 || enteredUserAge.trim().length === 0){
             // Update Error and Finish Function Execution
             setError({title: "Empty Value(s)", message: "Please fill in every value"});
             return;
         }
         
         // +enteredAge transforms enteredAge from string to number
-        if(+enteredAge < 1){
+        if(+enteredUserAge < 1){
             // Update error and finish execution if age isn't a valid number
             setError({title: "Invalid Age", message: "Please enter a valid age"});
             return;
         }
         
-        props.onAddUser(enteredUserName, enteredAge);
-        setEnteredUserName('');
-        setEnteredAge('');
+        props.onAddUser(enteredName, enteredUserAge);
+
+        // Reset values directly through the DOM (BAD PRACTICE)
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
+
+        // setEnteredUserName('');
+        // setEnteredAge('');
+
+        // We won't use states anymore, as we use refs to get the values
 
     }
 
@@ -34,13 +49,13 @@ const AddUser = (props) => {
         setError(null);
     }
 
-    const usernameChangeHandler = (event) => {
-        setEnteredUserName(event.target.value);
-    }
+    // const usernameChangeHandler = (event) => {
+    //     setEnteredUserName(event.target.value);
+    // }
 
-    const ageChangeHandler = (event) => {
-        setEnteredAge(event.target.value);
-    }
+    // const ageChangeHandler = (event) => {
+    //     setEnteredAge(event.target.value);
+    // }
 
     return(
     <div>
@@ -49,9 +64,9 @@ const AddUser = (props) => {
         <Card className = {styles.input}>
             <form onSubmit={addUserEventHandler}>
                 <label htmlFor="username">Username</label>
-                <input id = "username" type="text" onChange = {usernameChangeHandler} value={enteredUserName}/>
+                <input id = "username" type="text" /*onChange = {usernameChangeHandler} value={enteredUserName} */ ref = {nameInputRef}/>
                 <label htmlFor="age">Age in years</label>
-                <input id = "age" type="number" onChange = {ageChangeHandler} value={enteredAge}/>
+                <input id = "age" type="number" /* onChange = {ageChangeHandler} value={enteredAge}*/ ref = {ageInputRef}/>
                 <Button type = "submit">
                     Add User
                 </Button>
